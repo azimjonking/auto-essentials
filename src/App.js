a9d24236e48043dfca427dad8ac167971227b8de
azimjonking <azimjonjalilov1301@mail.ru>
2024-09-17 11:01:36 +0300

    nested route changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import "./App.css";
 
-import { Routes, Route } from "react-router-dom";
-
-import Header from "./components/header/Header";
-import Navbar from "./components/navbar/Navbar";
-import Footer from "./components/footer/Footer";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 // import pages
 import Home from "./pages/home/Home";
@@ -20,35 +16,69 @@ import Description from "./components/productDesc/Description";
 import AddInfo from "./components/productDesc/AddInfo";
 import Review from "./components/productDesc/Review";
 
+// import layout
+import AppLayout from "./layout/AppLayout";
+
 // import custom hooks
 import { useFetch } from "./hooks/useFetch";
 
 function App() {
   const topProductsData = useFetch("http://localhost:3000/topProducts");
 
+  const routes = createBrowserRouter([
+    {
+      path: "/",
+      element: <AppLayout />,
+      children: [
+        {
+          path: "/",
+          element: <Home topProductsData={topProductsData} />,
+        },
+        {
+          path: "shop",
+          element: <Shop />,
+        },
+        {
+          path: "contact",
+          element: <Contact />,
+        },
+        {
+          path: "myaccount",
+          element: <Myaccount />,
+        },
+        {
+          path: "cart",
+          element: <Cart />,
+        },
+        {
+          path: "checkout",
+          element: <Checkout />,
+        },
+        {
+          path: "details",
+          element: <Details />,
+          children: [
+            {
+              index: true,
+              element: <Description />,
+            },
+            {
+              path: "addinfo",
+              element: <AddInfo />,
+            },
+            {
+              path: "reviews",
+              element: <Review />,
+            },
+          ],
+        },
+      ],
+    },
+  ]);
+
   return (
     <div className="App">
-      <Header />
-      <Navbar />
-      <main>
-        <Routes>
-          <Route
-            path="/"
-            element={<Home topProductsData={topProductsData} />}
-          />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/myaccount" element={<Myaccount />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/details" element={<Details />}>
-            <Route index={true} element={<Description />} />
-            <Route path="addinfo" element={<AddInfo />} />
-            <Route path="reviews" element={<Review />} />
-          </Route>
-        </Routes>
-      </main>
-      <Footer />
+      <RouterProvider router={routes} />
     </div>
   );
 }
